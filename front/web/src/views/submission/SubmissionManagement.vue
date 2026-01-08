0b3d0a1faf479511108436a21accfe461d9dfe96
hardtochill <791094647@qq.com>
2026-01-08 17:26:12 +0800

    投稿模块-前端基础环境搭建

---
+++ front/web/src/views/submission/SubmissionManagement.vue
@@ -0,0 +1,14 @@
+<template>
+    <div>
+        <h1>投稿管理模块</h1>
+    </div>
+</template>
+
+<script setup>
+import {} from "@/api/submission/submissionPlan"
+
+</script>
+
+<style scoped>
+
+</style>
