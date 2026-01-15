b59fa06d16b17a33fa77b42de39b01f3f6ad770e
hardtochill <791094647@qq.com>
2026-01-15 20:03:33 +0800

    投稿计划模块-删除投稿计划时级联删除流程及关联文件

--- backend/java/src/main/java/com/ruoyi/experiment/service/impl/SubmissionPlanServiceImpl.java
+++ backend/java/src/main/java/com/ruoyi/experiment/service/impl/SubmissionPlanServiceImpl.java
@@ -1,5 +1,6 @@
 package com.ruoyi.experiment.service.impl;
 
+import com.ruoyi.common.exception.ServiceException;
 import com.ruoyi.common.utils.SecurityUtils;
 import com.ruoyi.common.utils.bean.BeanUtils;
 import com.ruoyi.experiment.constant.SubmissionConstants;
@@ -8,13 +9,18 @@ import com.ruoyi.experiment.enums.SubmissionPlanStatusEnum;
 import com.ruoyi.experiment.enums.SubmissionProcessStatusEnum;
 import com.ruoyi.experiment.enums.UserGraduateFlagEnum;
 import com.ruoyi.experiment.mapper.SubmissionPlanMapper;
+import com.ruoyi.experiment.mapper.SubmissionProcessFileMapper;
 import com.ruoyi.experiment.mapper.SubmissionProcessMapper;
 import com.ruoyi.experiment.pojo.dto.SubmissionPlanDTO;
 import com.ruoyi.experiment.pojo.dto.SubmissionPlanQueryDTO;
 import com.ruoyi.experiment.pojo.entity.SubmissionPlan;
 import com.ruoyi.experiment.pojo.entity.SubmissionProcess;
+import com.ruoyi.experiment.pojo.entity.SubmissionProcessFile;
 import com.ruoyi.experiment.pojo.vo.SubmissionPlanVO;
+import com.ruoyi.experiment.pojo.vo.SubmissionProcessVO;
 import com.ruoyi.experiment.service.SubmissionPlanService;
+import com.ruoyi.experiment.utils.FileUtils;
+import com.ruoyi.framework.config.ExperimentConfig;
 import com.ruoyi.project.system.domain.dto.UserForSelectQueryDTO;
 import com.ruoyi.project.system.domain.vo.UserForSelectVO;
 import com.ruoyi.project.system.mapper.SysUserMapper;
@@ -23,8 +29,12 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.time.LocalDateTime;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 @Slf4j
@@ -32,7 +42,9 @@ import java.util.List;
 public class SubmissionPlanServiceImpl implements SubmissionPlanService {
     private final SubmissionPlanMapper submissionPlanMapper;
     private final SubmissionProcessMapper submissionProcessMapper;
+    private final SubmissionProcessFileMapper submissionProcessFileMapper;
     private final SysUserMapper userMapper;
+    private final ExperimentConfig experimentConfig;
 
     @Override
     public List<SubmissionPlan> listSubmissionPlans(SubmissionPlanQueryDTO submissionPlanQueryDTO) {
@@ -81,13 +93,27 @@ public class SubmissionPlanServiceImpl implements SubmissionPlanService {
     @Override
     @Transactional(rollbackFor = Exception.class)
     public void deleteSubmissionPlan(Long id) {
-        // 删除投稿计划的所有流程
-        submissionProcessMapper.deleteByPlanId(id);
-
-        // todo todo 删除投稿流程的关联文件，本地文件+数据库记录
-
-        // 删除投稿计划
+        // 1.查出所有关联投稿流程
+        List<SubmissionProcessVO> submissionProcessList = submissionProcessMapper.selectByPlanId(id);
+        // 2.查出所有关联文件
+        List<SubmissionProcessFile> submissionProcessFileList = submissionProcessFileMapper.selectByProcessIds(submissionProcessList.stream().map(SubmissionProcessVO::getId).collect(Collectors.toList()));
+        // 3.删除投稿计划
         submissionPlanMapper.deleteById(id);
+        // 4.删除所有关联投稿流程
+        submissionProcessMapper.deleteByPlanId(id);
+        // 5.删除所有关联文件
+        submissionProcessFileMapper.deleteByIds(submissionProcessFileList.stream().map(SubmissionProcessFile::getId).collect(Collectors.toList()));
+        for (SubmissionProcessFile submissionProcessFile : submissionProcessFileList) {
+            Path filePath = Paths.get(FileUtils.getFileAbsolutePath(experimentConfig.getSubmissionBaseDir(), submissionProcessFile.getFilePath()));
+            try {
+                if (Files.exists(filePath)) {
+                    Files.delete(filePath);
+                }
+            } catch (Exception e) {
+                log.error("文件删除失败", e);
+                throw new ServiceException("文件删除失败");
+            }
+        }
     }
 
     @Override
